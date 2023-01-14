c29f0c1520cf67208d1458ea18f2df81e150927f
yasminalashin <117509486+yasminalashin@users.noreply.github.com>
2023-01-14 17:23:30 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -1,13 +1,3 @@
-var btnn = document.getElementsByClassName("iconn");
-
-function Toggle1() {
-  if (btnn.style.color == "black") {
-    btnn.style.color = "red";
-  } else {
-    btnn.style.color = "black";
-  }
-}
-
 const cart = document.querySelector("#cart");
 const cartModalOverlay = document.querySelector(".cart-modal-overlay");
 
@@ -43,11 +33,249 @@ for (var i = 0; i < addToCart.length; i++) {
   button.addEventListener("click", addToCartClicked);
 }
 
+var toggle = [];
+for (let i = 0; i < 25; i++) {
+  toggle.push(document.getElementById(`icon${i + 1}`));
+}
+console.log(toggle);
+
+// for (let i = 0; i < icon_button.length; i++) {
+//   var i_button = icon_button[i];
+//   i_button.addEventListener("click", Toggle);
+// }
+function Toggle(id) {
+  switch (id) {
+    case 0:
+      {
+        if (toggle[0].style.color == "black") {
+          toggle[0].style.color = "red";
+        } else {
+          toggle[0].style.color = "black";
+        }
+      }
+      break;
+    case 1:
+      {
+        if (toggle[1].style.color == "black") {
+          toggle[1].style.color = "red";
+        } else {
+          toggle[1].style.color = "black";
+        }
+      }
+      break;
+
+    case 2:
+      {
+        if (toggle[2].style.color == "black") {
+          toggle[2].style.color = "red";
+        } else {
+          toggle[2].style.color = "black";
+        }
+      }
+      break;
+    case 3:
+      {
+        if (toggle[3].style.color == "black") {
+          toggle[3].style.color = "red";
+        } else {
+          toggle[3].style.color = "black";
+        }
+      }
+      break;
+    case 4:
+      {
+        if (toggle[4].style.color == "black") {
+          toggle[4].style.color = "red";
+        } else {
+          toggle[4].style.color = "black";
+        }
+      }
+      break;
+    case 5:
+      {
+        if (toggle[5].style.color == "black") {
+          toggle[5].style.color = "red";
+        } else {
+          toggle[6].style.color = "black";
+        }
+      }
+      break;
+    case 6:
+      {
+        if (toggle[6].style.color == "black") {
+          toggle[6].style.color = "red";
+        } else {
+          toggle[6].style.color = "black";
+        }
+      }
+      break;
+    case 7:
+      {
+        if (toggle[7].style.color == "black") {
+          toggle[7].style.color = "red";
+        } else {
+          toggle[7].style.color = "black";
+        }
+      }
+      break;
+    case 8:
+      {
+        if (toggle[8].style.color == "black") {
+          toggle[8].style.color = "red";
+        } else {
+          toggle[8].style.color = "black";
+        }
+      }
+      break;
+    case 9:
+      {
+        if (toggle[9].style.color == "black") {
+          toggle[9].style.color = "red";
+        } else {
+          toggle[9].style.color = "black";
+        }
+      }
+      break;
+    case 10:
+      {
+        if (toggle[10].style.color == "black") {
+          toggle[10].style.color = "red";
+        } else {
+          toggle[10].style.color = "black";
+        }
+      }
+      break;
+    case 11:
+      {
+        if (toggle[11].style.color == "black") {
+          toggle[11].style.color = "red";
+        } else {
+          toggle[11].style.color = "black";
+        }
+      }
+      break;
+    case 12:
+      {
+        if (toggle[12].style.color == "black") {
+          toggle[12].style.color = "red";
+        } else {
+          toggle[12].style.color = "black";
+        }
+      }
+      break;
+    case 13:
+      {
+        if (toggle[13].style.color == "black") {
+          toggle[13].style.color = "red";
+        } else {
+          toggle[13].style.color = "black";
+        }
+      }
+      break;
+    case 14:
+      {
+        if (toggle[14].style.color == "black") {
+          toggle[14].style.color = "red";
+        } else {
+          toggle[14].style.color = "black";
+        }
+      }
+      break;
+    case 15:
+      {
+        if (toggle[15].style.color == "black") {
+          toggle[15].style.color = "red";
+        } else {
+          toggle[15].style.color = "black";
+        }
+      }
+      break;
+    case 16:
+      {
+        if (toggle[16].style.color == "black") {
+          toggle[16].style.color = "red";
+        } else {
+          toggle[16].style.color = "black";
+        }
+      }
+      break;
+    case 17:
+      {
+        if (toggle[17].style.color == "black") {
+          toggle[17].style.color = "red";
+        } else {
+          toggle[17].style.color = "black";
+        }
+      }
+      break;
+    case 18:
+      {
+        if (toggle[18].style.color == "black") {
+          toggle[18].style.color = "red";
+        } else {
+          toggle[18].style.color = "black";
+        }
+      }
+      break;
+    case 19:
+      {
+        if (toggle[19].style.color == "black") {
+          toggle[19].style.color = "red";
+        } else {
+          toggle[19].style.color = "black";
+        }
+      }
+      break;
+    case 20:
+      {
+        if (toggle[20].style.color == "black") {
+          toggle[20].style.color = "red";
+        } else {
+          toggle[20].style.color = "black";
+        }
+      }
+      break;
+    case 21:
+      {
+        if (toggle[21].style.color == "black") {
+          toggle[21].style.color = "red";
+        } else {
+          toggle[21].style.color = "black";
+        }
+      }
+      break;
+    case 22:
+      {
+        if (toggle[22].style.color == "black") {
+          toggle[22].style.color = "red";
+        } else {
+          toggle[22].style.color = "black";
+        }
+      }
+      break;
+    case 23:
+      {
+        if (toggle[23].style.color == "black") {
+          toggle[23].style.color = "red";
+        } else {
+          toggle[23].style.color = "black";
+        }
+      }
+      break;
+    case 24: {
+      if (toggle[24].style.color == "black") {
+        toggle[24].style.color = "red";
+      } else {
+        toggle[24].style.color = "black";
+      }
+    }
+  }
+}
+
 function addToCartClicked(event) {
   button = event.target;
   var cartItem = button.parentElement;
   var price = cartItem.getElementsByClassName("product-price")[0].innerText;
-
   var imageSrc = cartItem.getElementsByClassName("product-image")[0].src;
   addItemToCart(price, imageSrc);
   updateCartPrice();
@@ -177,5 +405,3 @@ function purchaseBtnClicked() {
 // cartsidebar.addEventListener("click",Function(){
 //   closecart.style.transform=(translate 0%)
 // })
-
-// cartcontent.addEventListener(fetch)
